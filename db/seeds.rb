9acf5e20a5d09da2c5ac1b5c77301b63d994f301
mohabashraf <mohabashraf466@gmail.com>
2018-10-17 22:19:44 +0200

    Created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,41 @@
 
 #Mohab Ashraf
 #16/10/2018
-10.time do|blog|
+10.times do|blog|
 	Blog.create!(
 		title: "Blog number #{blog}",
 		body:  "sdjskjdbjkdsj"
 	)
 end 
 puts "Blogs created"
-10.time do|skill|
+10.times do|skill|
 	Skill.create!(
 		title: "Rails #{skill}",
 		percentage:  75
 	)
 end
-puts skill created
+puts "skill created"
+5.times do |portofolio|
+	Portofolio.create!(
+		title: "dummy_title #{portofolio}",
+		subtitle: "dummmy subtitle",
+		body: "But I must explain to you how all this mistaken idea of denouncing
+		 			pleasure and praising pain was born and I will give you a complete 
+		 			account of the system,and expound the actual teachings of the 
+		 			great explorer of the truth, the master-builder of human happiness.
+		 			No one rejects, dislikes, or avoids pleasure itself,
+		 			because it is pleasure, but because those who do not know how to 
+		 			pursue pleasure rationally encounter consequences that are 
+		 			extremely painful. Nor again is there anyone who loves or pursues 
+		 			or desires to obtain pain of itself, because it is pain,
+		 		  but because occasionally circumstances occur in 
+		 		  which toil and pain can procure him some great pleasure.
+		 		  To take a trivial example, which of us ever undertakes laborious 
+		 		  physical exercise, except to obtain some advantage from it? 
+		 		  But who has any right to find fault with a man who chooses to enjoy
+		 		  a pleasure that has no annoying consequences,or one who avoids a pain that produces
+		 		   no resultant pleasure?",
+		 main_image: "https://via.placeholder.com/500x500",
+		 thumb_image: "https://via.placeholder.com/350x150"
+	)
+end
