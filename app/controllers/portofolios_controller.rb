6100a47a5ed0b346ce907eb607c831a6f8f59021
mohabashraf <mohabashraf466@gmail.com>
2018-10-18 02:06:24 +0200

    Integrated new and create for Portfolio

--- app/controllers/portofolios_controller.rb
+++ app/controllers/portofolios_controller.rb
@@ -1,6 +1,19 @@
 class PortofoliosController < ApplicationController
 	def index
-		@portfolios_items = Portofolio.all
-		puts @portfolios_items 
+		@portfolio_items = Portofolio.all
 	end
+	def new
+		@portfolio_item = Portofolio.new 
+	end
+	def create
+		portfolio_params = params.require(:portofolio).permit(:title, :body)
+    @portfolio_item = Portofolio.new(portfolio_params)
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to @portfolio_item, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
